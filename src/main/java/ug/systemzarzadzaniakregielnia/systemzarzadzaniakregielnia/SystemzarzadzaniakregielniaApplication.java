5b1dcd4b51105c7ee231f1f82eaae30d6d1ca23d
infernus0520 <infernus0520@gmail.com>
2017-11-07 21:04:21 +0100

    init

---
+++ src/main/java/ug/systemzarzadzaniakregielnia/systemzarzadzaniakregielnia/SystemzarzadzaniakregielniaApplication.java
@@ -0,0 +1,12 @@
+package ug.systemzarzadzaniakregielnia.systemzarzadzaniakregielnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemzarzadzaniakregielniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemzarzadzaniakregielniaApplication.class, args);
+	}
+}
---
+++ src/test/java/ug/systemzarzadzaniakregielnia/systemzarzadzaniakregielnia/SystemzarzadzaniakregielniaApplicationTests.java
@@ -0,0 +1,16 @@
+package ug.systemzarzadzaniakregielnia.systemzarzadzaniakregielnia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SystemzarzadzaniakregielniaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
